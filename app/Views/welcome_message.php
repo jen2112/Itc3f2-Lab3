ff3eb24b92d4d1f3e151e77428a2350596b9ceb2
Ramirez, Jennifer M <144778693+jen2112@users.noreply.github.com>
2023-10-08 22:56:04 +0800

    main

--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -1,15 +1,12 @@
-<?= $this->include('include/top')?>
-
-<div class="container-scroller">
-            <div class="d-flex align-items-center justify-content-between">
-              <a href="https://www.bootstrapdash.com/product/purple-bootstrap-admin-template/"><i class="mdi mdi-home me-3 text-white"></i></a>
-              <button id="bannerClose" class="btn border-0 p-0">
-                <i class="mdi mdi-close text-white me-0"></i>
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
-              <?= $this->include('include/header')?>
-              <?= $this->include('include/sidebar')?>
+<?= $this->include('includes/top')?>
+<div class="hero_area">
+    <?= $this->include('includes/header')?>
+    <?= $this->include('includes/sidebar')?>
+    <?= $this->include('includes/body')?>
+    <?= $this->include('includes/clientsection')?>
+    <?= $this->include('includes/giftsection')?>
+    <?= $this->include('includes/sidesection')?>
+    <?= $this->include('includes/whysection')?>
+    <?= $this->include('includes/info')?>
+    <?= $this->include('includes/footer')?>
 </div>
